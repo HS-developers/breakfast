8c9199eee08def8ff5964d5647914edf814c3b92
HS-developers <h.azaim1977@gmail.com>
2025-08-03 20:18:39 +0300

    Update bm.js

--- api/bm.js
+++ api/bm.js
@@ -1,1 +1,9 @@
-
+export default async function handler(req, res) {
+  const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+  const url = 'https://banquemisr.com/en/Home/CAPITAL-MARKETS/Exchange-Rates-and-Currencies';
+  const bmRes = await fetch(url);
+  const html = await bmRes.text();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.status(200).send(html);
+}
